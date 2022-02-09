d3b29c1515bcacf9d8b3792082918e7ff9cf9349
Karina Kumykova <k.kumykova@gmail.com>
2022-02-09 12:00:04 +0000

    handle test_two_days_later test

--- time_calculator.py
+++ time_calculator.py
@@ -19,11 +19,37 @@ def add_time(start, duration, starting_day=False):
     # minutes_sum = int(start_time_to_minutes) + int(duration_minutes)
 
     new_time = ''
-    if duration == "0:00":#str(duration_hours) == "0" and str(duration_minutes) == "00":
+    one_day_minutes = 24 * 60
+
+    if duration == "0:00":
         new_time += start
 
     elif duration == "24:00":
         new_time += start + " (next day)"
+
+    elif total_duration_minutes > one_day_minutes:
+        # nr of days later
+        days_count = (total_duration_minutes + time_till_am_pm_swap) / one_day_minutes
+        nr_of_days = 0
+        if days_count != round(days_count):
+            nr_of_days = round(days_count + 1)
+        print("DAYS COUNT", nr_of_days)
+
+        am_pm_in_duration_hours = (duration_hours / 6)
+        current_am_pm = start_time_split[1]
+        am_pm = ''
+        if current_am_pm == "PM" and am_pm_in_duration_hours % 2 == 0:
+            am_pm += " AM"
+        else:
+            am_pm += " PM"
+        hours = duration_hours % nr_of_days
+        minutes = "0" + str((total_duration_minutes - time_till_am_pm_swap) - duration_h_to_minutes)
+
+        if hours == 0:
+            new_time += "12:" + minutes + am_pm + " (" + str(nr_of_days) + " days later)"
+        else:
+            new_time += hours + ":" + minutes + am_pm + " (" + str(nr_of_days) + " days later)"
+
 
     elif start_time_split[1] == "PM" and time_till_am_pm_swap < total_duration_minutes:
         new_time_hours = int((total_duration_minutes - time_till_am_pm_swap) / 60)
@@ -50,10 +76,6 @@ def add_time(start, duration, starting_day=False):
         else:
             new_time += str(int(new_time_hours)) + ":" + str(new_time_minutes) + " PM"
 
-
-
-
-
     elif start_time_split[1] == "PM" and time_till_am_pm_swap > total_duration_minutes:
         total_minutes = int((total_duration_minutes + start_time_to_minutes))
         new_time_hours = int(total_minutes / 60)
@@ -63,43 +85,3 @@ def add_time(start, duration, starting_day=False):
         new_time += str(new_time_hours) + ":" + str(new_time_minutes) + " PM"
 
     return new_time
-
-    # # else:
-    #     new_time_hours = int(start_time_split[0].split(":")[0]) + int(duration_split[0].split(":")[0])
-    #     print("new_time_hours", new_time_hours)
-    #     minutes = int(start_time_split[0].split(":")[1]) + int(duration_split[0].split(":")[1])
-    #     if len(str(minutes)) == 2:
-    #         new_time_minutes = minutes
-    #     else:
-    #         new_time_minutes = "0" + str(minutes)
-
-    # hours_sum = int(start_hour) + int(duration_hour)
-    # minutes_sum = int(start_minutes) + int(duration_minutes)
-    # print("HOURS", hours_sum)
-    # print("MINUTES", minutes_sum)
-
-    # new_time = ''
-    # new_time_check = ''
-    # new_time_check += str(hours_sum) + ":" + str(minutes_sum)
-    # # print("new_time_check::", new_time_check)
-    # if start_am_pm == "PM" and int(hours_sum) < 13:
-    #     new_time += new_time_check + " PM"
-    # else:
-    #     trans_time = new_time_check.split(':')
-    #     trans_hours = int(trans_time[0])*60
-    #     trans_minutes = trans_hours + int(trans_time[1])
-    #     trans_start_time_to_minutes = int(start_hour)*60 + int(start_minutes)
-    #     # print("trans_minutes", trans_minutes)
-    #     # print(" trans_start_time_to_minutes",  trans_start_time_to_minutes)
-    #     min_dif = trans_minutes - trans_start_time_to_minutes
-    #     new_hours = round(min_dif/60)
-    #     # print("new_hours", new_hours)
-    #     new_minutes = (min_dif - 3*60) - (60 - int(start_minutes))
-    #     if len(str(new_minutes)) == 2:
-    #         new_time += str(new_hours) + ":" + str(new_minutes) + " PM"
-    #
-    #
-    #
-    #
-    #     else:
-    #         new_time += str(new_hours) + ":" + "0" + str(new_minutes) + " PM"
